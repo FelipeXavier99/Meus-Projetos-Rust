e3c74740ab74b5d3854a646974732c242ad73a20
Felipe Xavier <felipe.cvxavier@gmail.com>
2023-09-08 09:55:04 -0300

    Update main.rs

--- RUST-Testes/src/main.rs
+++ RUST-Testes/src/main.rs
@@ -12,10 +12,26 @@ struct Pessoa{
 
 }
 fn main() {
+    // Criando uma Option que contém um valor inteiro
+    let some_number: Option<i32> = Some(42);
+
+    // Criando uma Option que não contém um valor
+    let no_number: Option<i32> = None;
+
+    // Usando match para lidar com Option
+    match some_number {
+        Some(value) => println!("Valor existente: {}", value),
+        None => println!("Nenhum valor."),
+    }
+
+   
+}
+
+
     /* 
     //importando Classe
     calculator::minha_funcao();
-*/
+
 let pessoa1 =Pessoa {
 
     nome: String::from("Zé"),
@@ -26,10 +42,11 @@ let pessoa1 =Pessoa {
 print!("Nome= {}",pessoa1.nome);
 print!("Idade= {}",pessoa1.idade);
 }
+*/
 
 /*
 //exemplo funcao
 fn teste(a: i32) {
     println!("Função: {}", a);
-}
+
  */
