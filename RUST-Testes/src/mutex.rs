1d73aebef457d6ec416ff55cb33d9f0984d6e3d5
Felipe Xavier <127893679+FelipeXavier99@users.noreply.github.com>
2023-10-07 15:00:10 -0300

    Update mutex.rs

--- RUST-Testes/src/mutex.rs
+++ RUST-Testes/src/mutex.rs
@@ -33,3 +33,50 @@ fn main() {
     let valor_final = *contador.lock().unwrap();
     println!("Valor final do contador: {}", valor_final);
 }
+
+
+
+Exemplo 2:
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+fn main() {
+    // Criar um Mutex compartilhado para um vetor de números
+    let numbers = Arc::new(Mutex::new(Vec::new()));
+
+    let mut handles = vec![];
+
+    for i in 0..5 {
+        let numbers = Arc::clone(&numbers);
+
+        // Crie uma nova thread
+        let handle = thread::spawn(move || {
+            // Bloqueie o Mutex para acessar o vetor de números
+            let mut data = numbers.lock().unwrap();
+
+            // Adicione um número ao vetor
+            data.push(i);
+
+            // O Mutex será automaticamente desbloqueado quando "data" sair do escopo
+        });
+
+        handles.push(handle);
+    }
+
+    // Aguarde todas as threads terminarem
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // Obtenha o vetor de números após todas as threads terem terminado
+    let final_numbers = numbers.lock().unwrap();
+    println!("Números finais: {:?}", *final_numbers);
+}
+
+
+/*
+Neste exemplo, estamos criando um Mutex compartilhado para um vetor de números. Cada thread bloqueia o Mutex antes de adicionar um número ao vetor. Isso garante que apenas uma thread por vez possa modificar o vetor, evitando possíveis condições de corrida.
+
+Ao final, obtemos o vetor de números após todas as threads terem terminado e imprimimos os números finais. O uso do Mutex garante que a modificação do vetor seja feita de forma segura, mesmo quando várias threads estão acessando o recurso compartilhado.
+
+*/
