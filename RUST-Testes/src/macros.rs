9720bbe73cf48e6a0457c7b4de57f353f3b4dedc
Felipe Xavier <127893679+FelipeXavier99@users.noreply.github.com>
2023-10-11 17:26:39 -0300

    Create macros.rs

---
+++ RUST-Testes/src/macros.rs
@@ -0,0 +1,31 @@
+//1)Macros Declarativas (Macros de Item):
+//Macros de item geram código Rust inteiro. Isso pode incluir declarações de funções, estruturas, 
+módulos, ou até mesmo outras macros.
+
+
+macro_rules! say_hello {
+    () => {
+        println!("Hello, World!");
+    };
+}
+
+fn main() {
+    say_hello!(); // Chama a macro para gerar código.
+}
+
+
+//-------------------------------------------------
+/*
+2)Macros de Procedimento (Macros de Expressão):
+Macros de procedimento geram código Rust em uma expressão.
+    Elas são definidas usando a palavra-chave macro. Em contraste com as macros de item, as macros de procedimento 
+se parecem mais com funções regulares, com argumentos e corpo.
+*/
+
+macro my_function() {
+    println!("This is a procedural macro function.");
+}
+
+fn main() {
+    my_function!(); // Chama a macro para gerar código em uma expressão.
+}
