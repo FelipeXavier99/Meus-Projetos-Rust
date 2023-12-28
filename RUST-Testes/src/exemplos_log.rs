526877d00f4ef8fb2cee098691a632e73107b3d3
Felipe Xavier <127893679+FelipeXavier99@users.noreply.github.com>
2023-12-28 10:14:55 -0300

    Update exemplos_log.rs

--- RUST-Testes/src/exemplos_log.rs
+++ RUST-Testes/src/exemplos_log.rs
@@ -15,3 +15,23 @@ fn main() {
 [dependencies]
 log = "0.4"
 env_logger = "0.10.1"
+
+
+//////////////////////-- SEGUNDA OPÇÃO(tive q usar num projeto pq o de cima nao funcionava)////
+
+use log::{info, warn, error};
+use simple_logger::SimpleLogger;
+
+fn main() {
+    // Configurar o logger
+    SimpleLogger::new().init().unwrap();
+
+    // Exemplos de mensagens de log
+    info!("Esta é uma mensagem de informação.");
+    warn!("Este é um aviso.");
+    error!("Este é um erro.");
+}
+
+DEPENDENCIAS:
+log = "0.4"
+simple_logger = "1.11.0"
